9c35419e53856b9707f0fbcde2f48197f6da8969
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-11-26 07:40:46 -0400

    Update form.blade.php

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -2,13 +2,13 @@
     <div class="col-md-12">
         
         <div class="form-group mb-2 mb20">
-            <label for="name" class="form-label">{{ __('Name') }}</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $user?->name) }}" id="name" placeholder="Name">
+            <label for="name" class="form-label">{{ __('Nombre') }}</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $user?->name) }}" id="name" placeholder="Nombre">
             {!! $errors->first('name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
-            <label for="email" class="form-label">{{ __('Email') }}</label>
-            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $user?->email) }}" id="email" placeholder="Email">
+            <label for="email" class="form-label">{{ __('Correo Electrónico') }}</label>
+            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $user?->email) }}" id="email" placeholder="Correo Electrónico">
             {!! $errors->first('email', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
@@ -18,22 +18,22 @@
         </div>
         <div class="form-group mb-2 mb20">
             <label for="cedula_identidad" class="form-label">{{ __('Cédula de identidad') }}</label>
-            <input type="text" name="cedula_identidad" class="form-control @error('cedula_identidad') is-invalid @enderror" value="{{ old('cedula_identidad', $user?->cedula_identidad) }}" id="cedula_identidad" placeholder="CI">
+            <input type="text" name="cedula_identidad" class="form-control @error('cedula_identidad') is-invalid @enderror" value="{{ old('cedula_identidad', $user?->cedula_identidad) }}" id="cedula_identidad" placeholder="C.I.">
             {!! $errors->first('cedula_identidad', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
-            <label for="password" class="form-label">{{ __('Password') }}</label>
-            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="{{ $user && $user->exists ? __('Leave blank to keep current') : __('Password') }}">
+            <label for="password" class="form-label">{{ __('Contraseña') }}</label>
+            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="{{ $user && $user->exists ? __('Dejar en blanco para mantener la actual') : __('Contraseña') }}">
             {!! $errors->first('password', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
         <div class="form-group mb-2 mb20">
-            <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
-            <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" placeholder="{{ __('Confirm Password') }}">
+            <label for="password_confirmation" class="form-label">{{ __('Confirmar Contraseña') }}</label>
+            <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" placeholder="{{ __('Confirmar Contraseña') }}">
             {!! $errors->first('password_confirmation', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
 
     </div>
     <div class="col-md-12 mt20 mt-2">
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+        <button type="submit" class="btn btn-primary">{{ __('Guardar') }}</button>
     </div>
 </div>
